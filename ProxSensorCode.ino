ecf9ebfc545a88648cd3448046308f21dc5e3f97
Sem De Carvalho pinto <65860384+cilio0@users.noreply.github.com>
2024-06-02 14:10:14 +0200

    Update ProxSensorCode.ino

--- ProxSensorCode.ino
+++ ProxSensorCode.ino
@@ -2,12 +2,18 @@
 
 Zumo32U4ProximitySensors proxSensors;
 
-// Setup functie die ervoor zorgt dat de programma runt
+// Setup functie die voor voornamelijk de bit rate zorgt.
 
 void setup(){
-  // the bit rate zetten wat 9600 zal zijn
+  // the bit rate (snelheid van communicatie) zetten wat 9600 (standaard) zal zijn 
   Serial.begin(9600)
 
 //prox sensor initializeren  - Dit kan eventueel naar 2 of 3 zijn afhankelijk of dit naar de linesensor gaat of niet.
 proxSensors.initThreeSensors();
+}
+
+
+// dit start de code.
+void loop(){
+  
 }
