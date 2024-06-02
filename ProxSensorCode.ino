c344e6e258de2d059662c2202543debb5d1c96a1
Sem De Carvalho pinto <65860384+cilio0@users.noreply.github.com>
2024-06-02 14:39:23 +0200

    Update ProxSensorCode.ino
    Nog een laatste edit voor Xbee toepassing :)

--- ProxSensorCode.ino
+++ ProxSensorCode.ino
@@ -1,22 +1,39 @@
 #include <Zumo32U4.h>
 
 Zumo32U4ProximitySensors proxSensors;
+Zumo32U4ButtonA buttonA;
+Zumo32U4ButtonB buttonB;
 
-// Setup functie die voor voornamelijk de bit rate zorgt.
+// Setup functie die voor voornamelijk voor de instellingen voor de loop zorgen.
 
 void setup(){
   // the bit rate (snelheid van communicatie) zetten wat 9600 (standaard) zal zijn 
   Serial.begin(9600)
 
+    Serial.println("Press Button A to start...");
+    while (!buttonA.getSingleDebouncedPress()){
+      // hij wacht hier totdat buttonA is ingedrukt en losgelaten.
+    }
+
+  Serial.printLn("Button A pressed, starting....");
+
 //prox sensor initializeren  - Dit kan eventueel naar 2 of 3 zijn afhankelijk of dit naar de linesensor gaat of niet.
 proxSensors.initThreeSensors();
+
+  Serial.println("Klik")
 }
 
 
-// dit start de code.
+// Zorgt voor programma loop
 void loop(){
-  
+if (buttonB.getSingleDebouncedPress()){
+  Serial.println("Button B pressed, stopping...");
+  while (true){
+    // stopt de programma als button B is gedrukt (infinite loop) - DUS HIJ BLIJFT TECHNISCH GEZIEN AAN!!
+  }
 }
+  
+
 
 
 //uitlezen van de prox sensor
@@ -44,3 +61,4 @@ Serial.print(RightSensor);
 
 // een kleine delay (zorgt voor betere functionaliteit omdat het niet constant iets moet lezen des te langer des te beter)
 delay(200);
+}
