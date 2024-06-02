0f713e4e786dd5079a2e2a0b9b896b8847063c42
Sem De Carvalho pinto <65860384+cilio0@users.noreply.github.com>
2024-06-02 14:26:01 +0200

    Update ProxSensorCode.ino
    aanpassingen voordat xbee toegepast wordt.

--- ProxSensorCode.ino
+++ ProxSensorCode.ino
@@ -19,21 +19,28 @@ void loop(){
 }
 
 
+//uitlezen van de prox sensor
 proxSensors.read()
 
+  //declaratie van de sensoren
   int LeftSensor = proxSensors.countsLeftWithLeftLeds(); // linker sensor
   int FrontLeftSensor = proxSensors.countsFrontWithLeftLeds(); // tussen links en middelste sensor
   int FrontRightSensor = proxSensors.countsFrontWithRightLeds(); // tussen middelste en rechter sensor
   int RightSensor = proxSensors.countsRightWithRightsLeds(); // rechter sensor
 
+
+//uitprinten van de sensoren en welke wat is.
 Serial.print("Left: ");
 Serial.print(leftSensor);
+Serial.print(" ");
 Serial.print("Front Left Sensor: ");
 Serial.print(FrontLeftSensor);
+Serial.print(" ");
 Serial.print("Front Right Sensor: ");
 Serial.print(frontRightSensor);
+Serial.print(" ");
 Serial.print("Right Sensor: ");
 Serial.print(RightSensor);
 
-// een kleine delay
+// een kleine delay (zorgt voor betere functionaliteit omdat het niet constant iets moet lezen des te langer des te beter)
 delay(200);
