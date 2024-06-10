122389e39bea6d6d6e57cea7b4fff5d15239c48a
Sem De Carvalho pinto <65860384+cilio0@users.noreply.github.com>
2024-06-10 11:03:35 +0200

    Update and rename ProxSensorCode.ino to Main.ino

---
+++ Main.ino
@@ -0,0 +1,12 @@
+#include "MainController.h"
+
+// Create an instance of MainController
+MainController mainController;
+
+void setup() {
+    mainController.setup(); // 
+}
+
+void loop() {
+    mainController.loop(); // 
+}
